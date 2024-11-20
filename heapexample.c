fccf7d8c693a0d586472501b5eae62e0b060b4ed
jonathanrg13 <jrebog01@estudiantes.unileon.es>
2024-11-20 11:16:21 +0100

    Add files via upload

---
+++ heapexample.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/types.h>
+
+struct s_data {
+    char buffer[64];
+};
+
+struct s_fp {
+    void (*fp)();
+};
+
+void f_entrar() {
+    printf("Pasando\n");
+}
+
+void f_espero_fuera() {
+    printf("Esperando fuera\n");
+}
+
+int main(int argc, char **argv) {
+    struct s_data *s_midat;
+    struct s_fp *f;
+
+    s_midat = malloc(sizeof(struct s_data));
+    f = malloc(sizeof(struct s_fp));
+    f->fp = f_espero_fuera;
+
+    printf("data: está en [%p], el puntero fp está en [%p]\n", s_midat, f);
+
+    strcpy(s_midat->buffer, argv[1]);
+
+    f->fp();
+
+    return 0;
+}
